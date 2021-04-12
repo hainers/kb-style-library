d3c63992290cffcb27a90ac2339590690f7f648b
hainers <59766738+hainers@users.noreply.github.com>
2021-04-12 11:54:54 +0300

    Modal implementation + login views

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,25 @@
 document.addEventListener('DOMContentLoaded', function(event) {
-    document.querySelector("#kbfirma").addEventListener("change", function() {
-        document.querySelector('.kb-register-company').classList.remove('d-none');
-        document.querySelector('.kb-register-private').classList.add('d-none');
-    });
 
-    document.querySelector("#kbera").addEventListener("change", function() {
-        document.querySelector('.kb-register-company').classList.add('d-none');
-        document.querySelector('.kb-register-private').classList.remove('d-none');
-    });
+    // Register page acount type switch/radio btns
+    var $radioFirma = document.querySelector("#kbfirma");
+    if ($radioFirma !== null) {
+        $radioFirma.addEventListener("change", function() {
+            document.querySelector('.kb-register-company').classList.remove('d-none');
+            document.querySelector('.kb-register-private').classList.add('d-none');
+        });
+    }
+    var $radioEra = document.querySelector("#kbera");
+    if ($radioEra !== null) {
+            $radioEra.addEventListener("change", function() {
+            document.querySelector('.kb-register-company').classList.add('d-none');
+            document.querySelector('.kb-register-private').classList.remove('d-none');
+        });
+    }
+
+    // Login page id, mobile-id,smart-id actions
+
+
+    // Modal init functionality
+    MicroModal.init();
+    console.log(MicroModal);
 });
