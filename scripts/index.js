4c4b3e160085145799655c1f9a9f41367179c5d3
hainers <59766738+hainers@users.noreply.github.com>
2021-04-16 14:22:39 +0300

    Upload header styles

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,34 @@
 document.addEventListener('DOMContentLoaded', function(event) {
 
     // Register page acount type switch/radio btns
-    var $radioFirma = document.querySelector("#kbfirma");
-    if ($radioFirma !== null) {
-        $radioFirma.addEventListener("change", function() {
-            document.querySelector('.kb-register-company').classList.remove('d-none');
-            document.querySelector('.kb-register-private').classList.add('d-none');
-        });
-    }
-    var $radioEra = document.querySelector("#kbera");
-    if ($radioEra !== null) {
-            $radioEra.addEventListener("change", function() {
-            document.querySelector('.kb-register-company').classList.add('d-none');
-            document.querySelector('.kb-register-private').classList.remove('d-none');
-        });
-    }
+    $('#kbfirma').on('change', function() {
+        $('.kb-register-company').removeClass('d-none');
+        $('.kb-register-private').addClass('d-none');
+    });
+
+    $('#kbera').on('change', function() {
+        $('.kb-register-company').addClass('d-none');
+        $('.kb-register-private').removeClass('d-none');
+    });
+
+    $(".js-kb-search-form__toggle, .js-kb-search-form__toggle-mobile").on('click', function() {
+        $('.kb-search-form__hidden-fields--detailed').toggleClass('open');
+    });
+
+
+    var $searchTabs = $('.kb-search-form-tabs__item');
+    $searchTabs.on('click', function() {
+        $searchTabs.removeClass('kb-search-form-tabs__item--active');
+        $(this).addClass('kb-search-form-tabs__item--active');
+    });
+
+    $('.js-kb-search-form__toggle-collapse').on('click', function() {
+        $('.kb-search-form__hidden-fields').removeClass('open');
+    });
+    $('.js-kb-search-form__toggle-saved').on('click', function() {
+        $('.kb-search-form__hidden-fields--saved').addClass('open');
+    });
+
 
     // Login page id, mobile-id,smart-id actions
 
